b6e3782d2db4ffd42eddd0211ed4414e9e9c961d
kokoball <kbp4154@naver.com>
2022-04-13 20:56:07 +0900

    :sparkles:  Feat: addRestaurant, changeRestaurantField action 추가

--- src/redux/actions/restaurantsActions.ts
+++ src/redux/actions/restaurantsActions.ts
@@ -1,11 +1,11 @@
-import { RestaurantsData } from '@types';
+import { ChangeData } from '@types';
 
-interface RestaurantProps {
-  restaurants: RestaurantsData;
-}
+interface RestaurantProps {}
 
 export enum ActionType {
   SET_RESTAURANTS = 'SET_RESTAURANTS',
+  ADD_RESTAURANTS = 'ADD_RESTAURANTS',
+  CHANGE_RESTAURANTS = 'CHANGE_RESTAURANTS',
 }
 
 export const setRestaurants = (restaurants: RestaurantProps) => {
@@ -17,6 +17,24 @@ export const setRestaurants = (restaurants: RestaurantProps) => {
   };
 };
 
-export type ActionSetRestaurants = ReturnType<typeof setRestaurants>;
+export const addRestaurant = () => {
+  return {
+    type: ActionType.ADD_RESTAURANTS,
+  };
+};
 
-export type Action = ActionSetRestaurants;
+export const changeRestaurantField = ({ name, value }: ChangeData) => {
+  return {
+    type: ActionType.CHANGE_RESTAURANTS,
+    payload: {
+      name,
+      value,
+    },
+  };
+};
+
+export type ActionSetRestaurants = ReturnType<typeof setRestaurants>;
+export type ActionAddRestaurant = ReturnType<typeof addRestaurant>;
+export type ActionChangeRestaurantField = ReturnType<typeof changeRestaurantField>;
+
+export type Action = ActionSetRestaurants | ActionAddRestaurant | ActionChangeRestaurantField;
--- src/redux/reducers/restaurants.ts
+++ src/redux/reducers/restaurants.ts
@@ -1,7 +1,11 @@
 import { Action, ActionType } from '@redux/actions/restaurantsActions';
 
+const initialRestaurant = { name: '', category: '', address: '' };
+
 const initialState = {
+  newId: 100,
   restaurants: [],
+  restaurant: initialRestaurant,
 };
 
 export const restaurantsReducer = (state = initialState, { type, payload }: Action) => {
@@ -11,6 +15,25 @@ export const restaurantsReducer = (state = initialState, { type, payload }: Acti
       return {
         ...state,
         restaurants,
+      };
+    }
+    case ActionType.CHANGE_RESTAURANTS: {
+      const { name, value } = payload;
+      return {
+        ...state,
+        restaurant: {
+          ...state.restaurant,
+          [name]: value,
+        },
+      };
+    }
+    case ActionType.ADD_RESTAURANTS: {
+      const { newId, restaurants, restaurant } = state;
+      return {
+        ...state,
+        newId: newId + 1,
+        restaurants: [...restaurants, { ...restaurant, id: newId }],
+        restaurant: initialRestaurant,
       };
     }
     default:
