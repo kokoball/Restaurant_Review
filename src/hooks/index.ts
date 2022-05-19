75bebca22daceef76b153dd8e17a0f4f2a5a61cd
kokoball <kbp4154@naver.com>
2022-05-19 17:28:52 +0900

    :sparkles:  Feat: fetchRegions hook 분리

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as fetchCategories } from './fetchCategories';
+export { default as fetchRegions } from './fetchRegions';
 export { default as useTypedSelector } from './useTypedSelector';
 export { default as useTypedDispatch } from './useTypedDispatch';
